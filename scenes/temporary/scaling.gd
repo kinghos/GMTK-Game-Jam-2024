aa93a82b0a0e21e47d127fc7c47a3667f21511c8
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-17 09:28:39 +0100

    move raycast closer to player

--- scenes/temporary/scaling.gd
+++ scenes/temporary/scaling.gd
@@ -17,25 +17,3 @@ func resize(new_size: Vector2):
 	
 	await resize_tween.finished
 	resize_tween = null
-
-
-#func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	#if event is InputEventMouseButton and event.is_pressed():
-		#if event.button_index == MOUSE_BUTTON_LEFT:
-			#scale_change = scale_interval
-		#elif event.button_index == MOUSE_BUTTON_RIGHT:
-			#scale_change = -scale_interval
-		#
-		#var new_scale = scale + Vector2(scale_change, scale_change)
-		#new_scale.x = max(new_scale.x, MIN_SIZE)
-		#new_scale.y = max(new_scale.y, MIN_SIZE)
-		#new_scale.x = min(new_scale.x, MAX_SIZE)
-		#new_scale.y = min(new_scale.y, MAX_SIZE)
-		#
-		#for i in range(20):
-			#scale + new_scale / 20
-		#
-		#var tween = get_tree().create_tween()
-		#tween.set_parallel()
-		#for i in get_children():
-			#tween.tween_property(i, "scale", new_scale, 0.25).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
