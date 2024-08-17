680d9bed898d75156a5adfcf699af091011ee171
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-08-17 18:41:56 +0100

    YIPEEEEE

--- scenes/levels/base_level.gd
+++ scenes/levels/base_level.gd
@@ -6,10 +6,16 @@ func _ready() -> void:
 	for i in get_tree().get_nodes_in_group("Enemies"):
 		i.connect("kill", kill_entity)
 
-func kill_entity(node, color):
+func kill_entity(node: Node, color: Color):
 	var death_anim = death.instantiate()
 	add_child(death_anim)
 	death_anim.global_position = node.global_position
 	var anim: AnimationPlayer = death_anim.get_node("AnimationPlayer")
+	anim.get_animation("death").track_set_key_value(3, 0, color)
+	
+	var particles: GPUParticles2D = death_anim.get_node("GPUParticles2D")
+	particles.process_material.color = color
+	
+	color.a = 0
 	anim.get_animation("death").track_set_key_value(3, 1, color)
 	anim.play("death")
--- scenes/utilities/death.gd
+++ scenes/utilities/death.gd
@@ -1,5 +1,1 @@
 extends Node2D
-
-func _ready() -> void:
-	print($AnimationPlayer.get_animation("death").find_track("Skull:modulate", 0))
-	print($AnimationPlayer.get_animation("death").track_find_key(3, 1.2))
