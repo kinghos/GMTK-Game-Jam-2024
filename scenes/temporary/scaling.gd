fb627b386c2d6510153b7adf876f6421f8884949
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-17 10:59:44 +0100

    scaling fixes

--- scenes/temporary/scaling.gd
+++ scenes/temporary/scaling.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+var hovering: bool = false
+
 var resize_tween: Tween = null
 
+func _ready():
+	$CollisionShape2D.scale = Vector2(0.2, 0.2)
+
 func resize(new_size: Vector2):
-	if resize_tween and resize_tween.is_running():
+	if (resize_tween and resize_tween.is_running()) or not hovering:
 		return
 		
 	resize_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
@@ -11,3 +16,9 @@ func resize(new_size: Vector2):
 	
 	await resize_tween.finished
 	resize_tween = null
+
+func _on_mouse_enter(_shape_idx: int):
+	hovering = true
+
+func _on_mouse_exit(_shape_idx: int):
+	hovering = false
