e36f485b317819d6f9d44fcbf1c8d621300cc8e6
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-08-17 21:10:37 +0100

    Added alert, occlusion

--- scenes/enemies/turret.gd
+++ scenes/enemies/turret.gd
@@ -2,12 +2,13 @@ extends BaseEnemy
 
 signal kill(node, color)
 @export var skull_color: Color = Color("726def")
+@export var initial_light_color: Color = Color("6862da")
+@export var alert_light_color: Color = Color("c00000")
 
 var player_dying: bool = false
 
 func _ready():
 	$AnimationPlayer.play("eyeball bob")
-	$Line2D.hide()
 
 func _process(_delta):
 	var collider: CollisionObject2D = null
@@ -16,14 +17,13 @@ func _process(_delta):
 	elif $RightCast.is_colliding():
 		collider = $RightCast.get_collider()
 	
-	if !collider or collider.get_name() != "Player":
-		return
-	
-	if not player_dying:
-		kill.emit(collider, collider.skull_color)
-	player_dying = true
-	await get_tree().create_timer(1.4).timeout
-	get_tree().reload_current_scene()
+	if collider and collider.get_name() == "Player":
+		if not player_dying:
+			kill.emit(collider, collider.skull_color)
+		player_dying = true
+		$PointLight2D.color = alert_light_color
+		await get_tree().create_timer(1.4).timeout
+		get_tree().reload_current_scene()
 
 func _on_kill_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Resizables"):
