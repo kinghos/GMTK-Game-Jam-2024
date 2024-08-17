cd0855822fbbd055d84548c98bddff4c5937f189
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-08-17 11:19:05 +0100

    Added collision masks

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -56,7 +56,7 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _process(_delta: float) -> void:
-	var mouse_pos = $Camera2D.get_local_mouse_position()
+	var mouse_pos = to_local(get_viewport().get_mouse_position())
 	
 	# Find difference of vectors and see if its less than the max radius
 	var vector_diff = mouse_pos - to_local(position)
