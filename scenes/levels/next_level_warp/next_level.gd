a4173573dd950bef25b0419da0b78b966455983d
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-18 14:33:17 +0100

    change penis michael to portal

--- scenes/levels/next_level_warp/next_level.gd
+++ scenes/levels/next_level_warp/next_level.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @export var next_level_path: String
-@export var flip_horizontal: bool = false
 
 var level_1_texture = preload("res://assets/graphics/doors/castle_entrance.png")
 
@@ -9,8 +8,6 @@ func _ready():
 	if $".".get_parent().name == "Level1":
 		$Sprite2D.texture = level_1_texture
 		$Sprite2D.scale = Vector2(0.5, 0.5)
-	if flip_horizontal:
-		$Sprite2D.flip_h = !$Sprite2D.flip_h
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
