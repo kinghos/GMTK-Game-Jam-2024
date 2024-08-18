0ee1e4f4c14a675995c1d16777117060615096c0
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-18 16:13:30 +0100

    godly rework of base_resizable's script

--- scenes/objects/base_resizable.gd
+++ scenes/objects/base_resizable.gd
@@ -1,6 +1,12 @@
+@tool
 extends Node2D
 
 signal mass_changed(new_mass: float)
+
+@export_enum("Small", "Default", "Medium", "Large") var initial_scale: String = "Default" :
+	set(value):
+		initial_scale = value
+		update_initial_state()
 
 var resize_tween: Tween = null
 var hovering: bool = false
@@ -8,12 +14,11 @@ var can_push_switch
 
 var _mass: float = 1.0 : set = set_mass
 
-var scale_presets: Dictionary = {
-	# scale: [mass, friction]
-	Vector2(0.1, 0.1): [0.5, 0.4],
-	Vector2(0.2, 0.2): [1, 0.4],
-	Vector2(0.5, 0.5): [2, 0.45],
-	Vector2(1.0, 1.0): [4, 1]
+const SCALE_PRESETS = {
+	"Small": {"scale": Vector2(0.1, 0.1), "mass": 0.5, "friction": 0.4},
+	"Default": {"scale": Vector2(0.2, 0.2), "mass": 1.0, "friction": 0.4},
+	"Medium": {"scale": Vector2(0.5, 0.5), "mass": 2.0, "friction": 0.45},
+	"Large": {"scale": Vector2(1.0, 1.0), "mass": 4.0, "friction": 1.0}
 }
 
 func set_mass(value: float) -> void:
@@ -22,34 +27,43 @@ func set_mass(value: float) -> void:
 		$".".mass = value
 		mass_changed.emit(_mass, self)
 
+func update_initial_state():
+	var initial_preset = SCALE_PRESETS.get(initial_scale)
+	$CollisionShape2D.scale = initial_preset["scale"]
+	set_mass(initial_preset["mass"])
+
 func _ready():
 	add_to_group("Resizables") # make sure it's in Resizables
-	$CollisionShape2D.scale = Vector2(0.2, 0.2)
-	set_mass($".".mass)
+	update_initial_state()
 
 func resize(current_scale: Vector2, direction: int):
-	if (resize_tween and resize_tween.is_running()) or not hovering or !scale_presets.has(current_scale):
+	var scales = []
+	var scale_keys = []
+
+	for key in SCALE_PRESETS.keys():
+		var scale = SCALE_PRESETS[key]["scale"]
+		scales.append(scale)
+		scale_keys.append(key)
+	
+	if (resize_tween and resize_tween.is_running()) or not hovering or not scales.has(current_scale):
 		return
-	
-	var new_values: Array
-	
-	var scales = scale_presets.keys()
+
 	var index = scales.find(current_scale)
 	index += direction
 	
+	var new_values: Array
 	if index >= 0 and index < scales.size():
 		var new_scale = scales[index]
-		new_values = [new_scale, scale_presets[new_scale][0], scale_presets[new_scale][1]]
-	else:
-		new_values = [current_scale, scale_presets[current_scale][0], scale_presets[current_scale][1]]
-	
-	resize_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
-	resize_tween.tween_property($CollisionShape2D, "scale", new_values[0], 1)
-	resize_tween.tween_property($".", "_mass", new_values[1], 1)
-	$".".physics_material_override.set_friction(new_values[2])
-	
-	await resize_tween.finished
-	resize_tween = null
+		var new_key = scale_keys[index]
+		new_values = [new_scale, SCALE_PRESETS[new_key]["mass"], SCALE_PRESETS[new_key]["friction"]]
+		
+		resize_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
+		resize_tween.tween_property($CollisionShape2D, "scale", new_values[0], 1)
+		resize_tween.tween_property($".", "_mass", new_values[1], 1)
+		$".".physics_material_override.set_friction(new_values[2])
+		
+		await resize_tween.finished
+		resize_tween = null
 
 func _on_mouse_enter(_shape_idx: int):
 	hovering = true
