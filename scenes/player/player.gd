32047e84564a0e69e966a10599c3a57ffd7f3d9a
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-18 17:12:17 +0100

    not working stuff :(

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -91,3 +91,21 @@ func push_objects():
 		var col = get_slide_collision(i)
 		if col.get_collider() is RigidBody2D:
 			col.get_collider().apply_central_force(col.get_normal() * -push_force)
+
+func _on_camera_area_entered(area: Area2D) -> void:
+	var collision_shape = area.get_node("CollisionShape2D")
+	var size = collision_shape.shape.size
+	
+	var view_size = get_viewport_rect().size
+	if size.y < view_size.y:
+		size.y = view_size.y
+	
+	if size.x < view_size.x:
+		size.x = view_size.x
+	
+	var cam = $Camera2D
+	cam.limit_top = collision_shape.global_position.y - size.y/2
+	cam.limit_left = collision_shape.global_position.x - size.x/2
+	
+	$Camera2D.limit_bottom = cam.limit_top + size.y
+	$Camera2D.limit_right = cam.limit_left + size.x
