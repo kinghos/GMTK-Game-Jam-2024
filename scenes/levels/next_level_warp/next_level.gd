9bef72acda91af24729e4fc6d5d4ddf3f3c4335d
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-18 18:04:11 +0100

    improvements

--- scenes/levels/next_level_warp/next_level.gd
+++ scenes/levels/next_level_warp/next_level.gd
@@ -7,7 +7,7 @@ var level_1_texture = preload("res://assets/graphics/doors/castle_entrance.png")
 func _ready():
 	if $".".get_parent().name == "Level1":
 		$Sprite2D.texture = level_1_texture
-		$Sprite2D.scale = Vector2(0.5, 0.5)
+		$Sprite2D.scale = Vector2(4, 4)
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
