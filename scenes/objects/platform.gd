6402f909c8b4f3452ad7b6ab44977bd634f52790
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-19 13:53:50 +0100

    platform works perfectly

--- scenes/objects/platform.gd
+++ scenes/objects/platform.gd
@@ -10,8 +10,8 @@ extends StaticBody2D
 		expanded_scale = value
 		update_expanded_scale(value)
 
-var hovering = false
-var resize_tween: Tween = null
+var hovering: bool = false
+var expanded: bool = false
 
 func update_initial_scale(new_scale):
 	$".".scale.x = new_scale
@@ -23,16 +23,22 @@ func _ready() -> void:
 	if not Engine.is_editor_hint():
 		$EditorSprite.visible = false
 
-#func resize(current_scale: Vector2, direction: int):
-	#resize_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
-	#resize_tween.tween_property($CollisionShape2D, "scale", sizes[0], 1)
-	#resize_tween.tween_property($".", "_mass", sizes[1], 1)
-	#$".".physics_material_override.set_friction(sizes[2])
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and not expanded:
+		resize()
 
-func _on_mouse_enter(_shape_idx: int):
-	$CollisionShape2D/Sprite2D.material.set_shader_parameter("width", 0.5)
-	hovering = true
+func resize():
+	var resize_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	var expanded_scale_vector = Vector2(expanded_scale, 1)
+	resize_tween.tween_property($".", "scale", expanded_scale_vector, 1)
+	$Sprite2D.material.set_shader_parameter("width", 0)
+	expanded = true
 
-func _on_mouse_exit(_shape_idx: int):
-	$CollisionShape2D/Sprite2D.material.set_shader_parameter("width", 0)
+func _on_mouse_entered() -> void:
+	if not expanded:
+		$Sprite2D.material.set_shader_parameter("width", 0.5)
+		hovering = true
+
+func _on_mouse_exited() -> void:
+	$Sprite2D.material.set_shader_parameter("width", 0)
 	hovering = false
