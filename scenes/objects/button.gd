9720d4a55aaa7e27a59bccde751ad314e7d7e3f7
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-08-19 12:52:45 +0100

    Functioning locked door

--- scenes/objects/button.gd
+++ scenes/objects/button.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 @onready var sprite_2d = $Sprite2D
+@export var color: Color
 signal pressed(switch_id)
 
 func _ready():
 	sprite_2d.animation = "up"
+	$ButtonMask.animation = "up"
+	$ButtonMask.set_self_modulate(color)
 
 func _on_body_entered(body):
 	if "can_push_switch" in body:
 		sprite_2d.animation = "down"
+		$ButtonMask.animation = "down"
 		pressed.emit(self)
