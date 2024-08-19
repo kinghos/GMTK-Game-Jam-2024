180e9c382e9770f1346cc0e4727e2a4fd3881a31
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-19 21:20:22 +0100

    camera bounds tool script

--- scenes/utilities/camera_bounds.gd
+++ scenes/utilities/camera_bounds.gd
@@ -1,8 +1,19 @@
+@tool
 extends Node2D
 class_name CameraBounds
 
-@export var size: Vector2 = Vector2(10, 10)
+@export var size: Vector2 = Vector2(10, 10) :
+	set(new_size):
+		size.x = new_size.x
+		size.y = new_size.y
+		update_size(size)
 @export var priority: int = 0
+
+func update_size(value):
+	$Sprite2D.scale = value
+
+func _ready() -> void:
+	update_size(size)
 
 func player_in_bounds() -> bool:
 	var player_pos = Globals.player_pos
