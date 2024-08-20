7a51a995528fd9b93877ddb321b6c86fdc792ba1
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-20 15:17:38 +0100

    tail signal set up

--- scenes/ui/ending_cutscene.gd
+++ scenes/ui/ending_cutscene.gd
@@ -4,3 +4,6 @@ const MUSIC = preload("res://assets/audio/Ending Cutscene.mp3")
 
 func _ready() -> void:
 	Music._play_music_level(MUSIC)
+
+func _on_tail_entered(body: Node2D) -> void:
+	print("gained tail!")
