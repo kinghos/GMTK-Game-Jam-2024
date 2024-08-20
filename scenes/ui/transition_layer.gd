984a73b3f9b0b3ba0c1c459155b76d220f705667
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2024-08-20 10:40:04 +0100

    minor

--- scenes/ui/transition_layer.gd
+++ scenes/ui/transition_layer.gd
@@ -1,14 +1,11 @@
 @tool
 extends CanvasLayer
 
-
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		visible = false
 	else:
 		visible = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
